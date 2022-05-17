156d1b2a7859ab5527c353d72214f6e5152afe6b
xhasu <ing.xhasu@gmail.com>
2022-05-17 18:04:16 -0500

    add GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import 'styles/application.css'
+import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script id="googletagmanager">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-NV4Q5VM');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default function Document() {
         <link rel="stylesheet" href="/css/fonts.css" />
       </Head>
       <body className="font-century min-w-[320px]">
+        <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NV4Q5VM" height="0" width="0" style="display:none;visibility:hidden"></iframe>`}} />
         <Main />
         <NextScript />
       </body>
